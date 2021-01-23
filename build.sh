10cef2029f334a8c70eb9fd76cbd83726da673db
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-01-23 20:24:47 +0100

    Optimize elm build

--- build.sh
+++ build.sh
@@ -7,10 +7,19 @@ echo "removing existing build folder ------>"
 rm -rv ./dist
 mkdir ./dist
 
+JS_UNMINIFIED="main.js"
+JS_MINIFIED="main.min.js"
+
 echo ""
 echo "elm make -------->"
 pushd elm > /dev/null
-elm make src/Main.elm --output ../dist/main.js --optimize
+elm make src/Main.elm --output ../dist/$JS_UNMINIFIED --optimize
+popd > /dev/null
+
+echo ""
+echo "optimize elm build -------->"
+pushd uglify > /dev/null
+./node_modules/.bin/uglifyjs ../dist/$JS_UNMINIFIED --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > ../dist/$JS_MINIFIED
 popd > /dev/null
 
 echo ""
