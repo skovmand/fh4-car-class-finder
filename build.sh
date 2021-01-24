33a3af5c3eda1b6b1baf00aa2dc52efc33f53621
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-01-24 16:24:10 +0100

    Adapt build.sh to use docs folder

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 echo "Building car classes."
 echo ""
 echo "removing existing build folder ------>"
-rm -rv ./dist
-mkdir ./dist
+rm -rv ./docs
+mkdir ./docs
 
 JS_UNMINIFIED="main.js"
 JS_MINIFIED="main.min.js"
@@ -13,23 +13,23 @@ JS_MINIFIED="main.min.js"
 echo ""
 echo "elm make -------->"
 pushd elm > /dev/null
-elm make src/Main.elm --output ../dist/$JS_UNMINIFIED --optimize
+elm make src/Main.elm --output ../docs/$JS_UNMINIFIED --optimize
 popd > /dev/null
 
 echo ""
 echo "optimize elm build -------->"
 pushd uglify > /dev/null
-./node_modules/.bin/uglifyjs ../dist/$JS_UNMINIFIED --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > ../dist/$JS_MINIFIED
+./node_modules/.bin/uglifyjs ../docs/$JS_UNMINIFIED --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > ../docs/$JS_MINIFIED
 popd > /dev/null
 
 echo ""
 echo "tailwind -------->"
 pushd tailwind > /dev/null
-NODE_ENV=production ./node_modules/.bin/postcss src/styles.css -o ../dist/styles.css
+NODE_ENV=production ./node_modules/.bin/postcss src/styles.css -o ../docs/styles.css
 popd > /dev/null
 
 echo ""
 echo "html ------------>"
-cp html/index.html dist/index.html
+cp html/index.html docs/index.html
 
 echo "done!"
